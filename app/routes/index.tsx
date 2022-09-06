81091f5e5d879daae927897f11c02afe286c8fc8
Lucy Tibbetts <lucilletibbetts@gmail.com>
2022-09-06 16:09:16 -0700

    initial

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.25", textAlign: "center" }}>
+      <h1>Desirely</h1>
+    </div>
+  );
+}
