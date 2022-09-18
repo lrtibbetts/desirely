6a49928300f54d5a597d7ba3122d8accb05af9d7
Lucy Tibbetts <lucilletibbetts@gmail.com>
2022-09-18 16:44:44 -0700

    basic form, session stuff

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,3 +1,4 @@
+import { createCookieSessionStorage, redirect } from "@remix-run/node";
 import bcrypt from "bcryptjs";
 
 import { prisma } from "~/db.server"
@@ -35,6 +36,30 @@ export async function login({email, password}: LoginForm) {
     return { id: user.id } // TODO: return type?
 }
 
-export async function createUserSession(userId: string) {
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+    throw new Error("SESSION_SECRET must be set");
+}
 
+// TODO: set NODE_ENV in production
+const storage = createCookieSessionStorage({
+    cookie: {
+        name: "desirely_session",
+        secure: process.env.NODE_ENV === "production",
+        secrets: [sessionSecret],
+        sameSite: "lax",
+        path: "/",
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        httpOnly: true
+    }
+})
+
+export async function createUserSession(userId: string, redirectTo: string) {
+    const session = await storage.getSession();
+    session.set("userId", userId);
+    return redirect(redirectTo, {
+        headers: {
+            "Set-Cookie": await storage.commitSession(session),
+        },
+    });
 }
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,8 +1,36 @@
+import { ActionFunction } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
 
+export const action: ActionFunction = async({ request }) => {
+    const form = await request.formData();
+
+    const email = form.get("email");
+    const password = form.get("password");
+}
+
+
+// TOOD: move CSS to stylesheet
 export default function LoginPage() {
+    const actionData = useActionData();
+
     return(
         <div>
-
+            <h1>Login</h1>
+            <Form method="post">
+                <div style={{display:"inline-block"}}>
+                    <p>
+                        <label>Email: </label>
+                        <input type="text" name="email"></input>
+                    </p>
+                    <p>
+                        <label>Password: </label>
+                        <input type="text" name="password"></input>
+                    </p>
+                    <p>
+                        <button type="submit" style={{fontFamily: "Courier New, monospace"}}>Login</button>
+                    </p>
+                </div>
+            </Form>
         </div>
     );
 }
