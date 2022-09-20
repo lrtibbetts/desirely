4e85f4c9168d55217beb8c8727e871af3a9b6946
Lucy Tibbetts <lucilletibbetts@gmail.com>
2022-09-20 09:45:48 -0700

    links

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,22 @@
+import { Link } from "@remix-run/react";
+
+// TODO: move CSS to stylesheet
 export default function Index() {
   return (
     <div style={{lineHeight: "1.25", textAlign: "center"}}>
-      <h1>Desirely</h1>
-      <div style={{display:"flex", alignItems: "center", flexDirection:"column"}}>
-        <button style={{width:"100px", marginBottom: "20px"}}>Login</button>
-        <button style={{width: "100px"}}>Register</button>
-      </div>
+        <h1>Desirely</h1>
+        <div style={{display:"flex", alignItems: "center", flexDirection:"column"}}>
+            <Link
+                to="/login"
+                >
+                    Login
+            </Link>
+            <Link
+                to="/login"
+                >
+                    Sign up
+            </Link>
+        </div>
     </div>
   );
 }
