1784fbeb20a096fe73df7a6801322cf6814ebb9a
Lucy Tibbetts <lucilletibbetts@gmail.com>
2022-09-21 11:43:00 -0700

    make login generic

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,12 +1,12 @@
-import { ActionFunction, json } from "@remix-run/node";
+import { ActionFunction } from "@remix-run/node";
 import { Form, Link, useActionData } from "@remix-run/react";
 
+import InputFieldWithError from "~/components/InputFieldWithError";
 import { createUserSession } from "~/models/session.server";
 import { login, LoginResult } from "~/models/user.server";
-import { badRequest, validateEmail, validatePassword } from "~/utils";
+import { ActionData, badRequest, validateEmail, validatePassword } from "~/utils";
 
-type ActionData = {
-    error?: string,
+interface LoginActionData extends ActionData {
     fieldErrors?: {
         email: string | undefined,
         password: string | undefined,
@@ -46,7 +46,7 @@ export const action: ActionFunction = async({ request }) => {
 
 // TOOD: move CSS to stylesheet
 export default function LoginPage() {
-    const actionData = useActionData<ActionData>();
+    const actionData = useActionData<LoginActionData>() as LoginActionData;
 
     // TODO: toggle for password visibility
     return(
@@ -54,34 +54,15 @@ export default function LoginPage() {
             <h1>Log in</h1>
             <Form method="post">
                 <div style={{display:"inline-block"}}>
-                    <p>
-                        <label>Email:
-                            <input 
-                                type="text" name="email" style={{width: "200px"}}
-                                aria-invalid={actionData?.fieldErrors?.email ? true : undefined}
-                                aria-errormessage={
-                                    actionData?.fieldErrors?.email ? "email-error" : undefined
-                                }/>
-                        </label>
-                        {actionData?.fieldErrors?.email ? (
-                            <p style={{fontSize: "small"}} role="alert" id="email-error">
-                                {actionData.fieldErrors.email}
-                            </p>) : null}
-                    </p>
-                    <p>
-                        <label>Password:
-                        <input 
-                            type="password" name="password" style={{width: "200px"}}
-                            aria-invalid={actionData?.fieldErrors?.password ? true : undefined}
-                            aria-errormessage={
-                                actionData?.fieldErrors?.email ? "password-error" : undefined
-                            }/>
-                         </label>
-                         {actionData?.fieldErrors?.password ? (
-                            <p style={{fontSize: "small"}} role="alert" id="password-error">
-                                {actionData.fieldErrors.password}
-                            </p>) : null}
-                    </p>
+                    <InputFieldWithError<LoginActionData>
+                        actionData={actionData}
+                        label="Email: "
+                        fieldName="email"/>
+                    <InputFieldWithError<LoginActionData>
+                        actionData={actionData}
+                        label="Password: "
+                        fieldName="password"
+                        isPassword={true}/>
                     <div>
                         {actionData?.error? (
                             <p style={{fontSize: "small"}}>
