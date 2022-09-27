5bd1a288630e2c23f738e4c9689c60e0a136b823
Lucy Tibbetts <lucilletibbetts@gmail.com>
2022-09-27 11:15:36 -0700

    optimistic ui on delete

--- app/components/WeeklyView.tsx
+++ app/components/WeeklyView.tsx
@@ -1,4 +1,4 @@
-import { Form } from "@remix-run/react";
+import { Form, useTransition } from "@remix-run/react";
 import { Habit } from "~/models/habit.server";
 import DailyView from "./DailyView";
 
@@ -11,32 +11,39 @@ export default function WeeklyView({ habit, days } : WeeklyViewProps) {
     const dayAbbreviations : Array<string> = [
         "M", "T", "W", "Th", "F", "S", "Su"
     ];
+
+    const transition = useTransition();
     
     // TODO: move inline CSS to stylesheet
     // TODO: positioning, hover for x
     // TODO: make habit editable
     return (
         <div key={habit.id}>
-            <div style={{marginTop: "25px", display: "flex"}}>
-                <Form method="post">
-                    <input type="hidden" name="action" value="delete"></input>
-                    <input type="hidden" name="id" value={habit.id.toString()}/>
-                    <button type="submit" className="day-button">
-                        <h3>x</h3>
-                    </button>
-                </Form>
-                <h3>{habit.habitName}</h3>
-            </div>
-            {days.map((date: Date) => (
-                <DailyView
-                    key={`${habit.id}-${date.toISOString()}`}
-                    date={date}
-                    habitId={habit.id}
-                    completed={habit.habitEntries.find((entry: {entryDate : Date}) => {
-                        return entry.entryDate.getTime() === date.getTime();
-                    }) ? true : false}
-                    dayAbbreviation={dayAbbreviations[days.indexOf(date)]}/>
-            ))}
+            {transition.submission?.formData.get("action") === "delete"
+            && transition.submission?.formData.get("id") === habit.id ? null :
+                <div>
+                    <div style={{marginTop: "25px", display: "flex"}}>
+                    <Form method="post">
+                        <input type="hidden" name="action" value="delete"></input>
+                        <input type="hidden" name="id" value={habit.id.toString()}/>
+                        <button type="submit" className="day-button">
+                            <h3>x</h3>
+                        </button>
+                    </Form>
+                    <h3>{habit.habitName}</h3>
+                    </div>
+                    {days.map((date: Date) => (
+                        <DailyView
+                            key={`${habit.id}-${date.toISOString()}`}
+                            date={date}
+                            habitId={habit.id}
+                            completed={habit.habitEntries.find((entry: {entryDate : Date}) => {
+                                return entry.entryDate.getTime() === date.getTime();
+                            }) ? true : false}
+                            dayAbbreviation={dayAbbreviations[days.indexOf(date)]}/>
+                    ))}
+                </div>
+            }
         </div>
     );
 }
